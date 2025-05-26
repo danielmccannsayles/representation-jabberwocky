945f0029ebcf34683f2f080004c7ce348eace429
Daniel <danielmccannsayles@gmail.com>
2025-05-26 21:14:39 +0000

    Realized I recreated batched_get_hiddens xd

--- repeng/reading_pipeline.py
+++ repeng/reading_pipeline.py
@@ -90,73 +90,6 @@ class RepReading:
 
         return rep_reader.transform(hidden_states, hidden_layers, component_index)
 
-    # Uses PCA
-    def get_directions(
-        self,
-        train_inputs: list[DatasetEntry],
-        hidden_layers: Union[str, int] = -1,
-        n_difference: int = 1,
-        batch_size: int = 8,
-        train_labels: List[int] = None,
-    ):
-        """Train a RepReader on the training data.
-        Args:
-            batch_size: batch size to use when getting hidden states
-            direction_method: string specifying the RepReader strategy for finding directions
-            direction_finder_kwargs: kwargs to pass to RepReader constructor
-        """
-
-        if not isinstance(hidden_layers, list):
-            assert isinstance(hidden_layers, int)
-            hidden_layers = [hidden_layers]
-
-        # initialize a DirectionFinder
-        direction_finder = PCARepReader()
-
-        # if relevant, get the hidden state data for training set
-        hidden_states = None
-        relative_hidden_states = None
-        if direction_finder.needs_hiddens:
-            # get raw hidden states for the train inputs
-            # the order is [positive, negative, positive, negative, ...]
-            train_strs = [s for ex in train_inputs for s in (ex.positive, ex.negative)]
-            hidden_states = batched_get_hiddens(
-                self.model,
-                self.tokenizer,
-                train_strs,
-                hidden_layers,
-                batch_size,
-            )
-
-            # get differences between pairs
-            relative_hidden_states = {k: np.copy(v) for k, v in hidden_states.items()}
-            for layer in hidden_layers:
-                for _ in range(n_difference):
-                    relative_hidden_states[layer] = (
-                        relative_hidden_states[layer][::2]
-                        - relative_hidden_states[layer][1::2]
-                    )
-
-        # get the directions
-        direction_finder.directions = direction_finder.get_rep_directions(
-            relative_hidden_states,
-            hidden_layers,
-        )
-        for layer in direction_finder.directions:
-            if isinstance(direction_finder.directions[layer], np.ndarray):
-                direction_finder.directions[layer] = direction_finder.directions[
-                    layer
-                ].astype(np.float32)
-
-        # TODO: fix train_labels. For now we just mock it
-        mock_train_labels = [[1, 0] for _ in train_inputs]
-        signs = direction_finder.get_signs(
-            hidden_states, mock_train_labels, hidden_layers
-        )
-        direction_finder.direction_signs = signs
-
-        return direction_finder
-
 
 # Taken from repeng
 def batched_get_hiddens(
@@ -168,10 +101,9 @@ def batched_get_hiddens(
     rep_token: Optional[int] = None,
 ) -> dict[int, np.ndarray]:
     """
-    Changed this to add a rep_token. This is necessary for the reader to work.
+    Changed this to add a rep_token. This is necessary when reading w/ the reader (getting H_test).
     If no rep token is passed it defaults to False, and uses the last non padding index
 
-    OLD desc:
     Using the given model and tokenizer, pass the inputs through the model and get the hidden
     states for each layer in `hidden_layers` for the last token.
 
@@ -191,7 +123,6 @@ def batched_get_hiddens(
             attention_mask = encoded_batch["attention_mask"]
 
             for i in range(len(batch)):
-                # rep token calculation (-1 defaults to )
                 last_non_padding_index = (
                     attention_mask[i].nonzero(as_tuple=True)[0][-1].item()
                 )
@@ -212,55 +143,6 @@ def batched_get_hiddens(
 
 
 #### Consolidated version
-
-
-### I'm going to flatten this pipeline into a few functions and get it working correctly
-def string_to_hiddens(
-    model: PreTrainedModel,
-    tokenizer: PreTrainedTokenizerBase,
-    train_strs: list[str],
-    hidden_layers: list[int],
-    batch_size: int,
-    rep_token: int = -1,
-):
-    """Batch inputs
-    Tokenize them & run them through the model to get the hidden states
-
-    I should be able to re-use this code, or combine it with the batched_get_hiddens.
-
-    The rep_token is not used at all by create_rep_reader FYI
-    """
-    hidden_states = {layer: [] for layer in hidden_layers}
-
-    for start in range(0, len(train_strs), batch_size):
-        # Tokenize the batch of train strings
-        toks = tokenizer(
-            train_strs[start : start + batch_size],
-            return_tensors="pt",
-            padding=True,
-        ).to(model.device)
-
-        # Get hidden states from model
-        with torch.no_grad():
-            hs = model(**toks, output_hidden_states=True)["hidden_states"]
-
-        # Add hidden states
-        # We go through the hidden layers
-        # Get the hidden states for that layer
-        # Shrink it down to the rep token ->
-        # Convert it to float()
-
-        for layer in hidden_layers:
-            layer_hiddens = hs[layer][
-                :, rep_token, :
-            ].detach()  # Only care about the hidden states rep_token->
-            if layer_hiddens.dtype == torch.bfloat16:
-                layer_hiddens = layer_hiddens.float()
-            hidden_states[layer].append(layer_hiddens.detach().cpu().numpy())
-
-    return {layer: np.vstack(chunks) for layer, chunks in hidden_states.items()}
-
-
 def create_rep_reader(
     model: PreTrainedModel,
     tokenizer,
@@ -287,7 +169,7 @@ def create_rep_reader(
     relative_hidden_states = None
 
     # get raw hidden states for the train inputs
-    hidden_states = string_to_hiddens(
+    hidden_states = batched_get_hiddens(
         model,
         tokenizer,
         train_strs,
