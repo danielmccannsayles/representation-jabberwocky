d3ba2c74fe51e7b1f7a7861747e3eac4e9ad4eef
Daniel <danielmccannsayles@gmail.com>
2025-05-28 03:19:30 +0000

    consolidated all functionality into the reader class!

--- repeng/reader.py
+++ repeng/reader.py
@@ -4,10 +4,70 @@ New strategy - implement the rep reading from the old code.
 """
 
 from itertools import islice
+from typing import Optional
 
 import numpy as np
 import torch
+import tqdm
 from sklearn.decomposition import PCA
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
+
+from repeng.extract import DatasetEntry
+
+
+### Helpers
+# Taken from repeng, slightly modified
+def batched_get_hiddens2(
+    model: PreTrainedModel,
+    tokenizer,
+    inputs: list[str],
+    hidden_layers: list[int],
+    batch_size: int,
+    rep_token: Optional[int] = None,
+    hide_progress: Optional[bool] = None,
+) -> dict[int, np.ndarray]:
+    """
+    Changed this to add a rep_token. This is necessary when reading w/ the reader (getting H_test).
+    If no rep token is passed it defaults to False, and uses the last non padding index
+
+    Also added hide_progress flag
+
+    Using the given model and tokenizer, pass the inputs through the model and get the hidden
+    states for each layer in `hidden_layers` for the last token.
+
+    Returns a dictionary from `hidden_layers` layer id to an numpy array of shape `(n_inputs, hidden_dim)`
+    """
+    batched_inputs = [
+        inputs[p : p + batch_size] for p in range(0, len(inputs), batch_size)
+    ]
+    hidden_states = {layer: [] for layer in hidden_layers}
+
+    with torch.no_grad():
+        for batch in tqdm.tqdm(batched_inputs, disable=hide_progress):
+            # get the last token, handling right padding if present
+            encoded_batch = tokenizer(batch, padding=True, return_tensors="pt")
+            encoded_batch = encoded_batch.to(model.device)
+            out = model(**encoded_batch, output_hidden_states=True)
+            attention_mask = encoded_batch["attention_mask"]
+
+            for i in range(len(batch)):
+                last_non_padding_index = (
+                    attention_mask[i].nonzero(as_tuple=True)[0][-1].item()
+                )
+                token_index = last_non_padding_index if rep_token is None else rep_token
+
+                for layer in hidden_layers:
+                    hidden_idx = layer + 1 if layer >= 0 else layer
+                    hidden_state = (
+                        out.hidden_states[hidden_idx][i][token_index]
+                        .cpu()
+                        .float()
+                        .numpy()
+                    )
+                    hidden_states[layer].append(hidden_state)
+            del out
+
+    return {k: np.vstack(v) for k, v in hidden_states.items()}
 
 
 # Taken from repeng.
@@ -33,9 +93,20 @@ def recenter(x, mean=None):
     return x - mean
 
 
+### Actual class!
 class PCARepReader:
-    def __init__(self, hidden_layers: list[int], n_components=1):
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizerBase,
+        hidden_layers: list[int],
+        n_components=1,
+    ):
         super().__init__()
+        # The model & tokenizer should stay the same, since they're used to intialize the reader
+        self.model = model
+        self.tokenizer = tokenizer
+
         self.n_components = n_components
         # These h_train_means are used in transform. I think these are like the
         self.H_train_means = {}
@@ -48,11 +119,6 @@ class PCARepReader:
 
         # This is used a lot but is always the same
         self._hidden_layers = hidden_layers
-
-    @property
-    def direction_signs(self):
-        """Used externally by rep_read()"""
-        return self._direction_signs
 
     def get_rep_directions(self, hidden_states):
         """Get PCA components for each layer"""
@@ -79,6 +145,7 @@ class PCARepReader:
         self._directions = directions
 
     def get_signs(self, hidden_states, train_labels):
+        """TODO: consolidate this. Why do we need to get signs?"""
         signs = {}
 
         for layer in self._hidden_layers:
@@ -134,7 +201,6 @@ class PCARepReader:
                     )
 
             signs[layer] = layer_signs
-
         self._direction_signs = signs
 
     def transform(self, hidden_states, component_index):
@@ -165,9 +231,98 @@ class PCARepReader:
             transformed_hidden_states[layer] = H_transformed.cpu().numpy()
         return transformed_hidden_states
 
-    def train_reader():
-        """TODO: make this"""
-        pass
+    def read(
+        self,
+        input: str,
+        mean_layers: range,
+        batch_size,
+        component_index=0,
+    ):
+        """The actual load bearing call!
+
+        Reads a string.
+        Goes through it and gets an h_test for each token ->. Then calculates scores according to rep_reader.
+        Takes in some input, runs it through the model, uses the rep_reader on it."""
+        input_ids = self.tokenizer.tokenize(input)
+        results = []
+
+        # For each position (token index) we need to make H_tests
+        for pos in range(len(input_ids)):
+            rep_token = -len(input_ids) + pos
+
+            hidden_states = batched_get_hiddens2(
+                self.model,
+                self.tokenizer,
+                inputs=[input],
+                hidden_layers=self._hidden_layers,
+                batch_size=batch_size,
+                rep_token=rep_token,
+                hide_progress=True,
+            )
+
+            H_tests = self.transform(hidden_states, component_index)
+
+            results.append(H_tests)
+
+        # Turn results into the scores & the mean scores
+        scores = []
+        score_means = []
+        for result in results:
+            mean_scores = []
+            normal_scores = []
+            for layer in self._hidden_layers:
+                normal_scores.append(result[layer][0] * self._direction_signs[layer][0])
+
+                if layer in mean_layers:
+                    mean_scores.append(
+                        result[layer][0] * self._direction_signs[layer][0]
+                    )
+
+            scores.append(normal_scores)
+            score_means.append(np.mean(mean_scores))
+
+        return (input_ids, scores, score_means)
+
+    def initialize(
+        self,
+        train_inputs: list[DatasetEntry],
+        n_difference: int = 1,
+        batch_size: int = 8,
+    ):
+        """Initializes the rep reader! Run this first"""
+        # Turn inputs into train & labels
+        train_strs = []
+        train_labels = []
+        for input in train_inputs:
+            if input.flip:
+                train_strs += [input.negative, input.positive]
+                train_labels.append([False, True])
+            else:
+                train_strs += [input.positive, input.negative]
+                train_labels.append([True, False])
+
+        # PCA needs hidden state data!
+        # get hidden states for the train inputs
+        hidden_states = batched_get_hiddens2(
+            self.model,
+            self.tokenizer,
+            train_strs,
+            self._hidden_layers,
+            batch_size,
+        )
+
+        # get differences between pairs
+        relative_hidden_states = {k: np.copy(v) for k, v in hidden_states.items()}
+        for layer in self._hidden_layers:
+            for _ in range(n_difference):
+                relative_hidden_states[layer] = (
+                    relative_hidden_states[layer][::2]
+                    - relative_hidden_states[layer][1::2]
+                )
+
+        # I'train' rep reader
+        self.get_rep_directions(relative_hidden_states)
+        self.get_signs(hidden_states, train_labels)
 
     def reset_reader():
         """TODO: make this"""
